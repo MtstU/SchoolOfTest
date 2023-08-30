e15e281c595be4ee2883ed691171e65290d94319
mihai <molodikm@gmail.com>
2023-08-30 09:45:44 +0300

    first task complete

--- src/main/java/HourMinuteDegree.java
+++ src/main/java/HourMinuteDegree.java
@@ -1,2 +1,2 @@
-package PACKAGE_NAME;public class HourMinuteDegree {
+public class HourMinuteDegree {
 }
--- src/main/java/MaxMinAvg.java
+++ src/main/java/MaxMinAvg.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class MaxMinAvg {
+public class MaxMinAvg {
+    public static void main(String[] args) {
+        int n = 100;
+        double[] array = new double[n];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Math.random();
+        }
+
+        double min = array[0];
+        double max = array[0];
+        double average = 0;
+        double sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            if(max < array[i]) {
+                max = array[i];
+            }
+            if (min > array[i]) {
+                min = array[i];
+            }
+            sum += array[i];
+        }
+        average = sum/ array.length;
+
+        System.out.println(min);
+        System.out.println(max);
+        System.out.println(average);
+    }
 }
