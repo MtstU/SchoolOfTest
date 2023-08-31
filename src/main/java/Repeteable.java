7f7ea7ae9d0d955f478095490de192846fda208f
mihai <molodikm@gmail.com>
2023-08-31 09:29:03 +0300

    second task complete

--- src/main/java/Repeteable.java
+++ src/main/java/Repeteable.java
@@ -1,2 +1,20 @@
+/**
+ * Программа должна найти и вывести повторяющийся символ в слове "Hello"
+ */
 public class Repeteable {
+    public static void main(String[] args) {
+
+        String string = "Hello";
+        char[] charArray = string.toCharArray();
+
+        System.out.println("Повторяющиеся символы:");
+        for (int i = 0; i < string.length(); i++) {
+            for (int j = i + 1; j < string.length(); j++) {
+                if (charArray[i] == charArray[j]) {
+                    System.out.println(charArray[j] + " ");
+                    break;
+                }
+            }
+        }
+    }
 }
